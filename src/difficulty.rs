0c1e32dee2a5bfcd74920ecd250b98be5a9491df
matgo1 <matveistarostin2707@gmail.com>
2026-05-27 23:44:30 +0300

    Made different difficulties

--- src/difficulty.rs
+++ src/difficulty.rs
@@ -8,24 +8,31 @@ pub enum Difficulties {
     Hard,
 }
 
-fn process_response(input: &str) -> Difficulties {
-    match input {
-        "1" => Difficulties::Easy,
-        "2" => Difficulties::Medium,
-        "3" => Difficulties::Hard,
+// Implementation of Difficulties
+impl Difficulties {
+    pub fn get_range(&self) -> std::ops::RangeInclusive<u32> {
+        // Get range depends on chosen difficulty
+        // Returns Range
+        match self {
+            Difficulties::Easy => 1..=50,
+            Difficulties::Medium => 1..=100,
+            Difficulties::Hard => 1..=500,
+        }
     }
 }
 
-pub fn set_difficulty() -> Result<(u32, u32), io::Error> {
+pub fn select_difficulty() -> Result<std::ops::RangeInclusive<u32>, io::Error> {
     loop {
+        // Loop in case of errors
         inout::ask_difficulty();
 
-        let user_dif = inout::get_user_input()?;
-        if user_dif.trim() == "0" {
-            inout::write_difficulties_manual();
-            continue;
-        } else {
-            process_response(&user_dif);
+        let choice = inout::get_user_input()?;
+
+        match choice.to_lowercase().as_str() {
+            "1" | "easy" => return Ok(Difficulties::Easy.get_range()),
+            "2" | "medium" => return Ok(Difficulties::Medium.get_range()),
+            "3" | "hard" => return Ok(Difficulties::Hard.get_range()),
+            _ => inout::difficulty_expl(),
         }
     }
 }
