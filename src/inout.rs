61ef2acd23067b2e36731e6b8f13e39987df5d5a
matgo1 <matveistarostin2707@gmail.com>
2026-05-27 22:59:12 +0300

    Added comments to inout.rs

--- src/inout.rs
+++ src/inout.rs
@@ -1,5 +1,6 @@
 use std::io;
 
+// Print start menu
 pub fn write_hello_message() {
     println!("===Random Number Game===\n");
     println!("Choose:");
@@ -8,6 +9,7 @@ pub fn write_hello_message() {
     println!("3. Quit")
 }
 
+// Function for reading user input
 pub fn get_user_input() -> Result<String, io::Error> {
     let mut user_choice = String::new();
 
@@ -15,10 +17,16 @@ pub fn get_user_input() -> Result<String, io::Error> {
     Ok(user_choice.trim().to_string())
 }
 
+// Ask user which difficulty he want
 pub fn ask_difficulty() {
     println!("Choose difficulty");
     println!("1. Easy");
     println!("2. Medium");
     println!("3. Hard");
     println!("0. Info")
+}
+
+// TODO: Add some manual here
+pub fn difficulty_expl() {
+    println!("TODO!")
 }
