e70ea9cdaeeaf87f5e97809a5febcadf8af385f3
matgo1 <matveistarostin2707@gmail.com>
2026-05-27 23:43:47 +0300

    added commends in main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,33 @@ mod random;
 mod stats;
 
 fn main() -> Result<(), io::Error> {
+    // Loop of etntry interface
     loop {
-        inout::write_hello_message();
-        let choice = inout::get_user_input()?;
+        inout::write_hello_message(); // Starting message with mode choice
 
+        let choice = inout::get_user_input()?; // Read user input
+
+        // Different functionality depending on user choice
         match choice.as_str() {
             "1" => {
-                println!("Starting game...")
+                println!("Starting game...") // Here will be started main game loop
             }
             "2" => {
-                println!("Writing stats...")
+                println!("Writing stats...") // Reading and writing stats from json
             }
             "3" => {
-                break;
+                break; // Quiting the app
             }
 
             "67" => {
-                println!("Fuck")
+                println!("Fuck") // Voice of mankind
             }
 
             _ => {
-                println!("Wrong input")
+                println!("Wrong input") // For fools
             }
         }
     }
 
-    Ok(())
+    Ok(()) // Return that everithing OK if everithing is OK
 }
