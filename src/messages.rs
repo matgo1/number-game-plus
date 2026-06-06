7a820cac69c6816ce60ae56d30dc6e139200d086
matgo1 <mathewstaran2707@gmail.com>
2026-06-06 23:21:20 +0300

    Fixed bugs with stacking text

--- src/messages.rs
+++ src/messages.rs
@@ -1,6 +1,7 @@
 pub const WELCOME: &str = "===Welcome to Number Guess Game===\n\n";
-pub const MAIN_MENU: &str = "Choose: \n1. Start Game\n2.Check Stats\n3.Exit\n>>>";
+pub const MAIN_MENU: &str = "\nChoose: \n1. Start Game\n2.Check Stats\n3.Exit\n>>>";
 pub const ASK_DIFFICULTY: &str = "\
+
 Choose:
     1. Easy
     2. Medium
@@ -9,6 +10,7 @@ Write '?' for more information
 >>> ";
 
 pub const EXPLAIN_DIFFICULTIES: &str = "\
+
 You have 3 difficulty levels:
     1. Easy. You have 7 attemts to guess the number from 1 to 50.
     2. Medium. You have 5 attemts to guess the number from 1 to 100.
