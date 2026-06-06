81f91483445fe4870f51d429799acf712dd4c105
matgo1 <mathewstaran2707@gmail.com>
2026-06-06 13:49:14 +0300

    Created 2 new constants

--- src/messages.rs
+++ src/messages.rs
@@ -1,2 +1,16 @@
 pub const WELCOME: &str = "===Welcome to Number Guess Game===\n\n";
 pub const MAIN_MENU: &str = "Choose: \n1. Start Game\n2.Check Stats\n3.Exit\n>>>";
+pub const ASK_DIFFICULTY: &str = "\
+Choose:
+    1. Easy
+    2. Medium
+    3. Hard
+Write '?' for more information
+>>> ";
+
+pub const EXPLAIN_DIFFICULTIES: &str = "\
+You have 3 difficulty levels:
+    1. Easy. You have 7 attemts to guess the number from 1 to 50.
+    2. Medium. You have 5 attemts to guess the number from 1 to 100.
+    3. Hard. You have 3 attemts to guess the number from 1 to 500
+Write 'q' to exit >>> ";
