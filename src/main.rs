4fc7f039098c5115d13f00e94c7509791ecdf335
matgo1 <mathewstaran2707@gmail.com>
2026-06-06 12:49:53 +0300

    Rewrote main.rs for new system
    For now without any functionality

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
-mod difficulty;
-mod game;
 mod inout;
 mod messages;
-mod random;
-mod stats;
 use messages::*;
 
-fn main() -> Result<(), std::io::Error> {
-    inout::prompt(WELCOME); // Welcome messages
-    inout::prompt(MAIN_MENU); // Display main menu
+use crate::inout::prompt;
 
-    let user_input = inout::read_line()?;
+fn main() -> Result<(), std::io::Error> {
+    inout::prompt(WELCOME)?; // Welcome messages
+
+    // Loop for checking user input
+    loop {
+        inout::prompt(MAIN_MENU)?;
+
+        // Read user input
+        let user_mode_choice = inout::read_line()?;
+
+        // Analyze user input
+        match user_mode_choice.trim().parse::<u8>() {
+            Ok(choice) => match choice {
+                1 => {
+                    inout::prompt("option 1 is chosen")?;
+                    break;
+                }
+                2 => {
+                    inout::prompt("option 2 is chosen")?;
+                    break;
+                }
+                3 => {
+                    inout::prompt("option 3 is chosen")?;
+                    break;
+                }
+                _ => {
+                    prompt("Please, choice a valid number")?;
+                }
+            },
+            Err(_) => {
+                inout::prompt("Please, eneter a number")?;
+            }
+        }
+    }
 
     Ok(())
 }
