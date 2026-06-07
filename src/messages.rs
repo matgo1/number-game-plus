7b5423a7f28a3ba6b785693b149a70b864215a99
matgo1 <mathewstaran2707@gmail.com>
2026-06-07 10:46:35 +0300

    Fixed bugs with displaying text

--- src/messages.rs
+++ src/messages.rs
@@ -1,8 +1,7 @@
-pub const WELCOME: &str = "===Welcome to Number Guess Game===\n\n";
+pub const WELCOME: &str = "===Welcome to Number Guess Game===\n";
 pub const MAIN_MENU: &str = "\nChoose: \n1. Start Game\n2.Check Stats\n3.Exit\n>>> ";
 pub const ASK_DIFFICULTY: &str = "\
-
-Choose:
+\nChoose:
     1. Easy
     2. Medium
     3. Hard
@@ -10,8 +9,7 @@ Write '?' for more information
 >>> ";
 
 pub const EXPLAIN_DIFFICULTIES: &str = "\
-
-You have 3 difficulty levels:
+\nYou have 3 difficulty levels:
     1. Easy. You have 7 attemts to guess the number from 1 to 50.
     2. Medium. You have 5 attemts to guess the number from 1 to 100.
     3. Hard. You have 3 attemts to guess the number from 1 to 500
