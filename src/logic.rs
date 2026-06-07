1e380d4034b05716fdd64a7fe24affcd8807c858
matgo1 <mathewstaran2707@gmail.com>
2026-06-07 10:46:53 +0300

    Developed core of game logic

--- src/logic.rs
+++ src/logic.rs
@@ -1,3 +1,53 @@
-use crate::random::generate_rundom_number;
+use crate::inout::prompt;
+use crate::random::generate_random_number;
+use crate::{difficulty, inout};
+use clearscreen::clear as clean;
+use std::cmp::Ordering;
 
-pub fn stat_game() {}
+pub fn start_game() -> Result<(), std::io::Error> {
+    let config = difficulty::select_difficulty()?;
+    clean().expect("Failed to clear the screen");
+
+    let min_num: u16 = *config.answers_range.start();
+    let max_num: u16 = *config.answers_range.end();
+    let secret_number = generate_random_number(&config.answers_range);
+
+    let max_tries = config.tries_count;
+    let mut tries: u8 = 0;
+
+    loop {
+        let cnt_tries = max_tries - tries;
+        inout::prompt(format!("You have {cnt_tries} tries\n").as_str())?;
+        inout::prompt(format!("Guess between {min_num} and {max_num}\n").as_str())?;
+        inout::prompt("Your guess >>> ")?;
+        let user_number = inout::read_line()?;
+
+        let user_number: u16 = match user_number.parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match user_number.cmp(&secret_number) {
+            Ordering::Less => {
+                clean().expect("Faied to clear the screen");
+                inout::prompt("Too small!\n")?;
+            }
+            Ordering::Greater => {
+                clean().expect("Faied to clear the screen");
+                inout::prompt("Too Big?\n")?;
+            }
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+        tries += 1;
+
+        if tries == max_tries {
+            prompt("You lost!")?;
+            break;
+        }
+    }
+    Ok(())
+}
